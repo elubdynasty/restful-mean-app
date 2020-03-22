36b63936dc91c0287b5070965fb8f76d4d26c853
Leonard Luzon <leonard_luzon2000@yahoo.com>
2020-03-21 17:55:15 -0700

    Imported an AuthModule in AppModule to get access from Login/Signup fuctionalities. Transferred component declarations, as well as FormsModule (w/c login/signup form is depended on).

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,36 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms'; //using framwork for NgModel directive
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { LoginComponent } from './auth/login/login.component';
-import { SignupComponent } from './auth/signup/signup.component';
 import { AngularMaterialModule } from './angular-material.module';
 import { AppRoutingModule } from './app-routing.module';
 import { PostsModule } from './posts.module';
 import { AuthInterceptor } from './auth/auth-interceptor';
 import { ErrorInterceptor } from './error-interceptor';
 import { ErrorComponent } from './error/error.component';
+import { AuthModule } from './auth/auth.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    LoginComponent,
-    SignupComponent,
     ErrorComponent,
   ],
   imports: [
 
-    FormsModule,
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     HttpClientModule,
     AngularMaterialModule,
-    PostsModule
+    PostsModule,
+    AuthModule
 
   ],
   //any collection of services is for providers.
