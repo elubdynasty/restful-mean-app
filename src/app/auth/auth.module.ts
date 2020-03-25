f222b936ce0398b5f6770dc1969806be04a6491b
Leonard Luzon <leonard_luzon2000@yahoo.com>
2020-03-24 19:27:04 -0700

    Imported AuthRoutingModule into main AuthModule so that AppRoutingModule can access the child module routes.

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -4,6 +4,7 @@ import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
 import { AngularMaterialModule } from '../angular-material.module';
 import { CommonModule } from '@angular/common';
+import { AuthRoutingModule } from './auth-routing.module';
 
 
 @NgModule({
@@ -16,7 +17,8 @@ import { CommonModule } from '@angular/common';
 
     CommonModule,
     AngularMaterialModule,
-    FormsModule
+    FormsModule,
+    AuthRoutingModule
 
   ]
 })
